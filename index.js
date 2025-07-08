e9f50cc294e14319189cf06c3bbfa43f7406907b
dayoung <vkvidizl@gmail.com>
2025-07-08 09:46:03 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log("후츠릿 구독 좋아요");
+// console.log(2+2);
+
+// 변수선언
+const youtube = "후츠릿"; //상수
+let comment = "구독 좋아요"; //변수
+
+console.log("youtube : ", youtube);
+console.log("comment : ", comment);
+
+comment = "알림설정";
+console.log("comment : ", comment);
