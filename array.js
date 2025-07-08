b9340324757ba56c2c9945f17f4ea6d0d186f8de
dayoung <vkvidizl@gmail.com>
2025-07-08 15:54:16 +0900

    for문 진행

--- array.js
+++ array.js
@@ -53,3 +53,46 @@ const studenList = [
 console.log("경덕님의 MBTI : ", studenList[0].mbti);
 console.log("관태님의 favoritFoods 1번 : ", studenList[1].favoritFoods[0]);
 console.log("마지막 학생의 이름 : ", studenList[studenList.length - 1].name);
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// 여기에 화살표 함수로 다시 작성하세요.
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(5, 3)); // 예상 결과: 8
+
+// 여기에 두 개의 parameter를 가진 화살표 함수를 작성하세요.
+const minus = (a, b) => {
+  return a - b;
+};
+
+// 함수 테스트
+console.log(minus(60, 20)); // 예상 결과: 40
+
+const introduce = (name, age) => {
+  return "안녕하세요, " + name + "님. 당신의 나이는 " + age + "세입니다.";
+};
+
+// 여기에 함수를 호출하는 코드를 작성하세요.// 당신의 이름과 나이를 argument로 사용하세요.
+console.log(introduce("김다영", "29")); // 예시 결과: "안녕하세요, 김철수님. 당신의 나이는 25세입니다."
+
+// 여기에 orderSandwich 함수를 작성하세요
+// orderSandwich 함수는 type 매개변수를 받고 타입에 따라 다른 재료를 반환해야 합니다
+
+const orderSandwich = (type = "basic") =>
+  type === "vegan" ? "🥖 + 🥬 + 🍅 + 🥑 + 🥒" : "🥖 + 🥬 + 🍅 + 🧀 + 🥓";
+
+// 함수 테스트 - 아래 코드는 수정하지 마세요
+console.log("기본 샌드위치 주문: " + orderSandwich());
+console.log("기본 샌드위치 주문: " + orderSandwich("basic"));
+console.log("비건 샌드위치 주문: " + orderSandwich("vegan"));
+
+/* 예상 결과:
+기본 샌드위치 주문: 🥖 + 🥬 + 🍅 + 🧀 + 🥓
+기본 샌드위치 주문: 🥖 + 🥬 + 🍅 + 🧀 + 🥓
+비건 샌드위치 주문: 🥖 + 🥬 + 🍅 + 🥑 + 🥒
+*/
